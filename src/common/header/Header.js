5237a51b2ba23e70c668994ec01a19efb7b53b2c
Gangadhard2 <gangadhar.d2009@gmail.com>
2019-07-28 18:09:43 -0700

    Added PropTypes for TabContainer and added a check for state value when displaying login TabContainer

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -5,6 +5,34 @@ import logo from '../../assets/logo.svg';
 import Modal from 'react-modal';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import PropTypes from 'prop-types';
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+    }
+};
+
+const TabContainer = function (props) {
+    return (
+        <Typography component="div" style={{ padding: 0, textAlign: 'center' }}>
+            {props.children}
+        </Typography>
+    )
+}
+
+TabContainer.propTypes = {
+    children: PropTypes.node.isRequired
+}
 
 class Header extends Component {
 
@@ -13,41 +41,61 @@ class Header extends Component {
         this.state = {
             modalIsOpen: false,
             value: 0
-        };
-
+        }
     }
-    openModalHandler = () => {
 
-        this.setState({ modalIsOpen: true })
+    openModalHandler = () => {
+        this.setState({ modalIsOpen: true });
     }
 
     closeModalHandler = () => {
-
-        this.setState({ modalIsOpen: false })
+        this.setState({ modalIsOpen: false });
     }
 
-    tabChangeHandler=(event,value)=>{
-
-         this.setState({value});
+    tabChangeHandler = (event, value) => {
+        this.setState({ value });
     }
 
     render() {
         return (
             <div>
                 <header className="app-header">
-                    <img src={logo} className="app-logo" alt="logo" />
+                    <img src={logo} className="app-logo" alt="Movies App Logo" />
                     <div className="login-button">
                         <Button variant="contained" color="default" onClick={this.openModalHandler}>
                             Login
                         </Button>
                     </div>
                 </header>
-                <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen} contentLabel="Login" onRequestClose={this.closeModalHandler}>
-                    <Tabs value={this.state.value} onChange={this.tabChangeHandler}>
+
+                <Modal
+                    ariaHideApp={false}
+                    isOpen={this.state.modalIsOpen}
+                    contentLabel="Login"
+                    onRequestClose={this.closeModalHandler}
+                    style={customStyles}
+
+                >
+                    <Tabs className="tabs" value={this.state.value} onChange={this.tabChangeHandler}>
                         <Tab label="Login" />
                         <Tab label="Register" />
                     </Tabs>
 
+                    {this.state.value === 0 &&
+                        <TabContainer>
+                            <FormControl required>
+                                <InputLabel htmlFor="username">Username</InputLabel>
+                                <Input id="username" type="text" />
+                            </FormControl>
+                            <br /><br />
+                            <FormControl required>
+                                <InputLabel htmlFor="password">Password</InputLabel>
+                                <Input id="password" type="password" />
+                            </FormControl>
+                            <br /><br />
+                            <Button variant="contained" color="primary">LOGIN</Button>
+                        </TabContainer>
+                    }
                 </Modal>
             </div>
         )
